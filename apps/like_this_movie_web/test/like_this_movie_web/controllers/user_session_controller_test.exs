22ebc94c8dc155ff49c9e6f29b77b31db0cb0783
Henrique Artur <contato@henriqueartur.com>
2023-10-04 21:30:40 -0300

    :white_check_mark: Fix tests after change redirects

--- apps/like_this_movie_web/test/like_this_movie_web/controllers/user_session_controller_test.exs
+++ apps/like_this_movie_web/test/like_this_movie_web/controllers/user_session_controller_test.exs
@@ -15,10 +15,10 @@ defmodule LikeThisMovieWeb.UserSessionControllerTest do
         })
 
       assert get_session(conn, :user_token)
-      assert redirected_to(conn) == ~p"/"
+      assert redirected_to(conn) == ~p"/logged"
 
       # Now do a logged in request and assert on the menu
-      conn = get(conn, ~p"/")
+      conn = get(conn, ~p"/logged")
       response = html_response(conn, 200)
       assert response =~ user.email
       assert response =~ ~p"/users/settings"
@@ -36,7 +36,7 @@ defmodule LikeThisMovieWeb.UserSessionControllerTest do
         })
 
       assert conn.resp_cookies["_like_this_movie_web_user_remember_me"]
-      assert redirected_to(conn) == ~p"/"
+      assert redirected_to(conn) == ~p"/logged"
     end
 
     test "logs the user in with return to", %{conn: conn, user: user} do
@@ -65,7 +65,7 @@ defmodule LikeThisMovieWeb.UserSessionControllerTest do
           }
         })
 
-      assert redirected_to(conn) == ~p"/"
+      assert redirected_to(conn) == ~p"/logged"
       assert Phoenix.Flash.get(conn.assigns.flash, :info) =~ "Account created successfully"
     end
 
@@ -91,7 +91,7 @@ defmodule LikeThisMovieWeb.UserSessionControllerTest do
         })
 
       assert Phoenix.Flash.get(conn.assigns.flash, :error) == "Invalid email or password"
-      assert redirected_to(conn) == ~p"/users/log_in"
+      assert redirected_to(conn) == ~p"/"
     end
   end
 
--- apps/like_this_movie_web/test/like_this_movie_web/live/user_login_live_test.exs
+++ apps/like_this_movie_web/test/like_this_movie_web/live/user_login_live_test.exs
@@ -6,7 +6,7 @@ defmodule LikeThisMovieWeb.UserLoginLiveTest do
 
   describe "Log in page" do
     test "renders log in page", %{conn: conn} do
-      {:ok, _lv, html} = live(conn, ~p"/users/log_in")
+      {:ok, _lv, html} = live(conn, ~p"/")
 
       assert html =~ "Log in"
       assert html =~ "Register"
@@ -17,8 +17,8 @@ defmodule LikeThisMovieWeb.UserLoginLiveTest do
       result =
         conn
         |> log_in_user(user_fixture())
-        |> live(~p"/users/log_in")
-        |> follow_redirect(conn, "/")
+        |> live(~p"/")
+        |> follow_redirect(conn, "/logged")
 
       assert {:ok, _conn} = result
     end
@@ -29,20 +29,20 @@ defmodule LikeThisMovieWeb.UserLoginLiveTest do
       password = "123456789abcd"
       user = user_fixture(%{password: password})
 
-      {:ok, lv, _html} = live(conn, ~p"/users/log_in")
+      {:ok, lv, _html} = live(conn, ~p"/")
 
       form =
         form(lv, "#login_form", user: %{email: user.email, password: password, remember_me: true})
 
       conn = submit_form(form, conn)
 
-      assert redirected_to(conn) == ~p"/"
+      assert redirected_to(conn) == ~p"/logged"
     end
 
     test "redirects to login page with a flash error if there are no valid credentials", %{
       conn: conn
     } do
-      {:ok, lv, _html} = live(conn, ~p"/users/log_in")
+      {:ok, lv, _html} = live(conn, ~p"/")
 
       form =
         form(lv, "#login_form",
@@ -53,19 +53,19 @@ defmodule LikeThisMovieWeb.UserLoginLiveTest do
 
       assert Phoenix.Flash.get(conn.assigns.flash, :error) == "Invalid email or password"
 
-      assert redirected_to(conn) == "/users/log_in"
+      assert redirected_to(conn) == "/"
     end
   end
 
   describe "login navigation" do
     test "redirects to registration page when the Register button is clicked", %{conn: conn} do
-      {:ok, lv, _html} = live(conn, ~p"/users/log_in")
+      {:ok, lv, _html} = live(conn, ~p"/")
 
       {:ok, _login_live, login_html} =
         lv
         |> element(~s|main a:fl-contains("Sign up")|)
         |> render_click()
-        |> follow_redirect(conn, ~p"/users/register")
+        |> follow_redirect(conn, ~p"/register")
 
       assert login_html =~ "Register"
     end
@@ -73,13 +73,13 @@ defmodule LikeThisMovieWeb.UserLoginLiveTest do
     test "redirects to forgot password page when the Forgot Password button is clicked", %{
       conn: conn
     } do
-      {:ok, lv, _html} = live(conn, ~p"/users/log_in")
+      {:ok, lv, _html} = live(conn, ~p"/")
 
       {:ok, conn} =
         lv
         |> element(~s|main a:fl-contains("Forgot your password?")|)
         |> render_click()
-        |> follow_redirect(conn, ~p"/users/reset_password")
+        |> follow_redirect(conn, ~p"/reset_password")
 
       assert conn.resp_body =~ "Forgot your password?"
     end
