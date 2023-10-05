e1dffc13014f169491e50d84581494d1815b7cba
Henrique Artur <contato@henriqueartur.com>
2023-10-04 21:58:18 -0300

    :bug: Fix register redirect test

--- apps/like_this_movie_web/test/like_this_movie_web/live/user_registration_live_test.exs
+++ apps/like_this_movie_web/test/like_this_movie_web/live/user_registration_live_test.exs
@@ -48,7 +48,7 @@ defmodule LikeThisMovieWeb.UserRegistrationLiveTest do
       assert redirected_to(conn) == ~p"/logged"
 
       # Now do a logged in request and assert on the menu
-      conn = get(conn, "/")
+      conn = get(conn, "/logged")
       response = html_response(conn, 200)
       assert response =~ email
       assert response =~ "Settings"
