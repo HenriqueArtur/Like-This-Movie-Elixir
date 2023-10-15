25a838c911040b4371e0693848e072691eae2aba
Henrique Artur <contato@henriqueartur.com>
2023-10-15 13:06:46 -0300

    :wrench: Add a :http_client to global application

--- config/config.exs
+++ config/config.exs
@@ -69,3 +69,9 @@ config :phoenix, :json_library, Jason
 # Import environment specific config. This must remain at the bottom
 # of this file so it overrides the configuration defined above.
 import_config "#{config_env()}.exs"
+
+# HTTP Client
+config :like_this_movie, :http_client, LikeThisMovie.HttpClientFinch
+
+# TMDB
+config :like_this_movie, :tmdb_token, System.get_env("TMDB_TOKEN")
