973638a7c86fea696f8bcb447e9c9fb85b2c7007
Henrique Artur <contato@henriqueartur.com>
2023-10-25 21:02:46 -0300

    :white_check_mark: Add a urls tests

--- apps/like_this_movie/test/infrastructure/service/tmdb/tmdb_test.exs
+++ apps/like_this_movie/test/infrastructure/service/tmdb/tmdb_test.exs
@@ -28,4 +28,13 @@ defmodule LikeThisMovie.TMDBTest do
     patch(@http_client, :get, {:error, %{reason: "some error"}})
     assert {:error, %{reason: "some error"}} == LikeThisMovie.TMDB.trending("day")
   end
+
+  test "return the url" do
+    assert "https://image.tmdb.org/t/p/w500" == LikeThisMovie.TMDB.url_img()
+  end
+
+  test "return the poster url" do
+    a_post_path = "/yqnNLn24shYnZ6kqGpbwuB3NJ0D.jpg"
+    assert "https://image.tmdb.org/t/p/w500" <> a_post_path == LikeThisMovie.TMDB.url_img_from(a_post_path)
+  end
 end
